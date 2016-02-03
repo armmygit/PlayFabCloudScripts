00e94ed933a6401c27d0232a9b9b71aabba49576
armmygit <ekkarat01@gmail.com>
2016-02-03 18:07:24 +0700

    update cloud

--- helloWorld.js
+++ helloWorld.js
@@ -4,3 +4,12 @@ handlers.helloWorld = function (args)
   log.info(message);
   return { messageValue: message };
 }
+
+handlers.userData = function (args)
+{
+  var result = server.GetUserAccountInfo({
+    PlayFabId: args.pId
+  });
+
+  return result;
+}
