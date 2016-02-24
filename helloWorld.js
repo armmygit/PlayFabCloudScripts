f7a21e459046321ff491b179850e9c6844a4cee8
armmybit <ekkarat01@gmail.com>
2016-02-24 16:26:19 +0700

    remove test

--- helloWorld.js
+++ helloWorld.js
@@ -16,11 +16,7 @@ handlers.SetCoin = function (args)
 	var data={};
 	data[args.code]=args.curCoin;
 	
-	server.UpdateUserData  ({
-		PlayFabId: currentPlayerId,
-		Data: data,
-		Permission: true
-	});
+	
 	
 	return result;
 }
