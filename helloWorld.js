852149668a66b2f7d70f196207f1ead47584554c
armmybit <ekkarat01@gmail.com>
2016-02-24 16:23:34 +0700

    add coin key

--- helloWorld.js
+++ helloWorld.js
@@ -1,17 +1,26 @@
 handlers.helloWorld = function (args)
 {
-  var message = "Hello " + args.name;
-  log.info(message);
-  return { messageValue: message };
+	var message = "Hello " + args.name;
+		log.info(message);
+	return { messageValue: message };
 }
 
 handlers.SetCoin = function (args)
 {
-  var result = server.AddUserVirtualCurrency({
-    PlayFabId: args.id,
-    VirtualCurrency: args.code,
-	Amount: args.amount
-  });
+	var result = server.AddUserVirtualCurrency({
+		PlayFabId: currentPlayerId,
+		VirtualCurrency: args.code,
+		Amount: args.amount
+	});
 
-  return result;
+	var data={};
+	data[args.code]=args.curCoin;
+	
+	server.UpdateUserData  ({
+		PlayFabId: currentPlayerId,
+		Data: data,
+		Permission: true
+	});
+	
+	return result;
 }
